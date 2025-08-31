fe18ed66685a0aa2c4c1b72c427903fed2f85dcb
andradasantiago1 <andradasantiago.004@gmail.com>
2025-08-31 16:52:32 -0300

    agrego user.routes

--- mi-primera-api/src/routes/book.routes.ts
+++ mi-primera-api/src/routes/book.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
 import * as bookController from '../controllers/book.controller';
+import { validate } from '../middlewares/validation.middleware';
+import { createBookSchema, updateBookSchema } from '../validations/book.validation';
 
 const router = Router();
 
-router.get('/', bookController.getAllBooks); 
+router.get('/', bookController.getAllBooks);
 router.get('/:id', bookController.getBookById);
-router.post('/', bookController.createBook); 
-router.put('/:id', bookController.updateBook); 
+router.get('/search', bookController.findBooks);
+router.post('/', validate(createBookSchema), bookController.createBook);
+router.put('/:id', validate(updateBookSchema), bookController.updateBook);
 router.delete('/:id', bookController.deleteBook);
-router.get('/search', bookController.findBooks); 
 
 export const bookRoutes = router;
---
+++ mi-primera-api/src/routes/user.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import * as userController from '../controllers/user.controller';
+import { validate } from '../middlewares/validation.middleware';
+import { createUserSchema, updateUserSchema } from '../validations/user.validation';
+
+const router = Router();
+
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUserById);
+router.post('/', validate(createUserSchema), userController.createUser);
+router.put('/:id', validate(updateUserSchema), userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+
+export const userRoutes = router;
