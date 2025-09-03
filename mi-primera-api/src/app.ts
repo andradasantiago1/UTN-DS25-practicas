5c1da5baf9cf2d41a647f61063ebfc12b5901f8e
andradasantiago1 <andradasantiago.004@gmail.com>
2025-09-03 17:52:47 -0300

    agrego .env.example, modifico el app.ts e instalo cors

--- mi-primera-api/src/app.ts
+++ mi-primera-api/src/app.ts
@@ -2,9 +2,19 @@ import express from 'express';
 import { bookRoutes } from './routes/book.routes';
 import { handleError } from './middlewares/error.middleware';
 import { logRequest } from './middlewares/logger.middleware';
-const app = express();
-const PORT = 3000;
+import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config(); 
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+const corsOptions = { 
+origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(logRequest);
 
