aa7f32668b74f7df4dbce9214a5520d54a8adada
andradasantiago1 <andradasantiago.004@gmail.com>
2025-09-13 08:58:12 -0300

    actualizo prisma con roles de usuario. Agrego auth.middleware y actualizo auth y user validations

--- mi-primera-api/src/validations/user.validation.ts
+++ mi-primera-api/src/validations/user.validation.ts
@@ -2,13 +2,18 @@ import { z } from 'zod';
 
 export const createUserSchema = z.object({
     email: z.string()
-    .email('Formato de email inválido'),
+    .email('Formato de email inválido')
+    .toLowerCase()
+    .trim(),
     password: z.string()
-    .min(9, 'La contraseña debe tener al menos 9 caracteres'),
+    .min(9, 'La contraseña debe tener al menos 9 caracteres')
+    .regex(/[A-Z]/, 'Debe contener al menos una mayúscula')
+    .regex(/[0-9]/, 'Debe contener al menos un número'),
     name: z.string()
     .min(1, 'El nombre es requerido')
     .max(150, 'El nombre no puede exceder los 150 caracteres')
     .trim(),
+    role: z.enum(['USER','ADMIN']).optional().default('USER')
 });
 
 export const updateUserSchema = createUserSchema.partial();
