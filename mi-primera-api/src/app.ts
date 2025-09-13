d6c378dd080c12d3a19eb02a9440f63b7cd8746f
andradasantiago1 <andradasantiago.004@gmail.com>
2025-09-13 09:47:37 -0300

    actualizo app.ts

--- mi-primera-api/src/app.ts
+++ mi-primera-api/src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
-import { bookRoutes } from './routes/book.routes';
-import { handleError } from './middlewares/error.middleware';
-import { logRequest } from './middlewares/logger.middleware';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { authRoutes } from './routes/auth.routes';
+import { bookRoutes } from './routes/book.routes';
+import { userRoutes } from './routes/user.routes';
+import { handleError } from './middlewares/error.middleware';
+import { logRequest } from './middlewares/logger.middleware';
+
 
 dotenv.config(); 
 const PORT = process.env.PORT || 3000;
@@ -11,14 +14,18 @@ const app = express();
 
 const corsOptions = { 
 origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-credentials: true
+credentials: true,
+methods: ['GET', 'POST', 'PUT', 'DELETE' , 'OPTIONS' ],
+allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(logRequest);
 
+app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(handleError);
 app.listen(PORT, () => {
