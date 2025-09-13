d5369c48983e1000dd282b728b3837c8dcbd22bd
andradasantiago1 <andradasantiago.004@gmail.com>
2025-09-13 09:43:05 -0300

    agrego auth.routes y validaciones en book.routes y user.routes

--- mi-primera-api/src/routes/book.routes.ts
+++ mi-primera-api/src/routes/book.routes.ts
@@ -2,14 +2,33 @@ import { Router } from 'express';
 import * as bookController from '../controllers/book.controller';
 import { validate } from '../middlewares/validation.middleware';
 import { createBookSchema, updateBookSchema } from '../validations/book.validation';
+import { authenticate, authorize } from '../middlewares/auth.middleware';
 
 const router = Router();
 
-router.get('/', bookController.getAllBooks);
-router.get('/:id', bookController.getBookById);
-router.get('/search', bookController.findBooks);
-router.post('/', validate(createBookSchema), bookController.createBook);
-router.put('/:id', validate(updateBookSchema), bookController.updateBook);
-router.delete('/:id', bookController.deleteBook);
+router.get('/'
+    authenticate, 
+    authorize('ADMIN','USER')
+    , bookController.getAllBooks);
+router.get('/:id',
+    authenticate,
+    authorize('ADMIN', 'USER'), 
+    bookController.getBookById);
+router.get('/search',
+    authenticate,
+    authorize('ADMIN', 'USER'),
+    bookController.findBooks);
+router.post('/',
+    authenticate,
+    authorize('ADMIN', 'USER'),
+    validate(createBookSchema), bookController.createBook);
+router.put('/:id',
+    authenticate,
+    authorize('ADMIN', 'USER'),
+    validate(updateBookSchema), bookController.updateBook);
+router.delete('/:id',
+    authenticate,
+    authorize('ADMIN'),
+    bookController.deleteBook);
 
 export const bookRoutes = router;
--- mi-primera-api/src/routes/user.routes.ts
+++ mi-primera-api/src/routes/user.routes.ts
@@ -1,14 +1,36 @@
 import { Router } from 'express';
 import * as userController from '../controllers/user.controller';
 import { validate } from '../middlewares/validation.middleware';
-import { createUserSchema, updateUserSchema } from '../validations/user.validation';
+import { authenticate, authorize } from '../middlewares/auth.middleware';
+import { createUserSchema, updateUserSchema } from
+'../validations/user.validation';
 
 const router = Router();
-
-router.get('/', userController.getAllUsers);
-router.get('/:id', userController.getUserById);
-router.post('/', validate(createUserSchema), userController.createUser);
-router.put('/:id', validate(updateUserSchema), userController.updateUser);
-router.delete('/:id', userController.deleteUser);
-
+router.get('/',
+    authenticate,
+    authorize('ADMIN'),
+    userController.getAllUsers
+);
+router.get('/:id',
+    authenticate,
+    authorize('ADMIN'),
+    userController.getUserById
+);
+router.post('/',
+    authenticate,
+    authorize('ADMIN'),
+    validate(createUserSchema),
+    userController.createUser
+);
+router.put('/:id',
+    authenticate,
+    authorize('ADMIN'),
+    validate(updateUserSchema),
+    userController.updateUser
+);
+router.delete('/:id',
+    authenticate,
+    authorize('ADMIN'),
+    userController.deleteUser
+);
 export const userRoutes = router;
